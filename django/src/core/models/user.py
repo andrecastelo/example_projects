bd5680480b357e67e914685299d9fb2aa5f878c8
Andre Castelo <andrecastelo@gmail.com>
2016-06-25 21:14:20 -0300

    inital commit

---
+++ django/src/core/models/user.py
@@ -0,0 +1,15 @@
+from django.db import models
+from core.models import BaseModel
+
+class User(BaseModel):
+    full_name = models.CharField(max_length=250, blank=False, verbose_name='Full Name')
+    username = models.CharField(max_length=15, unique=True, verbose_name='Username')
+    password = models.CharField(max_length=128, verbose_name='Password')
+    email = models.CharField(max_length=128, unique=True, verbose_name='Email')
+    description = models.TextField(verbose_name='Description')
+    website = models.CharField(max_length=128, verbose_name='Website')
+    active = models.BooleanField(default=False, verbose_name='Active')
+
+    # country
+    # website
+    # active
