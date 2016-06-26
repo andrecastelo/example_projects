b596f91d48de41ef22e2087180a6f87012e2abb5
Andre Castelo <andrecastelo@gmail.com>
2016-06-26 16:07:30 -0300

    added tag and tweet models + migrations

--- django/src/core/models/__init__.py
+++ django/src/core/models/__init__.py
@@ -1,2 +1,4 @@
 from .base import BaseModel
 from .user import User
+from .tag import Tag
+from .tweet import Tweet
---
+++ django/src/core/models/tag.py
@@ -0,0 +1,5 @@
+from django.db import models
+from core.models import BaseModel, User
+
+class Tag(BaseModel):
+    pass
