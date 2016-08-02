c8ee962494c700fa7e724e5543dd4216d7b3e6ea
Andre Castelo <andrecastelo@gmail.com>
2016-08-02 01:06:54 -0300

    Added login endpoint and altered my User class to replace django's

--- django/src/core/models/user.py
+++ django/src/core/models/user.py
@@ -1,12 +1,13 @@
+from django.utils import timezone
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 from django_countries.fields import CountryField
 from django.contrib.auth.hashers import is_password_usable
 
 from core.models import BaseModel
 
 
-class User(AbstractBaseUser, BaseModel):
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
     name = models.CharField(max_length=250, blank=False, verbose_name='Name')
     username = models.CharField(max_length=30, unique=True, verbose_name='Username')
     email = models.CharField(max_length=128, unique=True, verbose_name='Email')
@@ -14,12 +15,32 @@ class User(AbstractBaseUser, BaseModel):
     website = models.CharField(max_length=128, verbose_name='Website')
     is_active = models.BooleanField(default=False, verbose_name='Active')
     country = CountryField(blank=True)
+    date_joined = models.DateTimeField('Date joined', default=timezone.now)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'email', 'name']
+    REQUIRED_FIELDS = ['email', 'name']
 
     def save(self, *args, **kwargs):
         if not is_password_usable(self.password):
             self.set_password(self.password)
 
         super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    # def email_user(self, subject, message, from_email=None):
+        # send_mail(subject, message, from_email, [self.email])
--- django/src/core/urls.py
+++ django/src/core/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views as rest_views
 
 from core import views
 
 urlpatterns = [
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^users$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)$', views.UserDetail.as_view()),
+    url(r'^login', rest_views.obtain_auth_token)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
